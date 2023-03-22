bfaa51611d2091446c54850a4aaf524116f9700b
Ashfaq <syashfaq@gmail.comgit statuscleargit config --global user.name Ashfaqgit branch feature/initialcommitgit statuspwdgit config --global user.email syashfaq@gmail.com>
2023-03-22 23:12:44 +0530

    commited complete

--- src/App.js
+++ src/App.js
@@ -1,44 +1,41 @@
-import ExpenseItem from './Components/ExpenseItem'
-import Expenses from './Components/Expenses';
-
+import ExpenseItem from "./Components/ExpenseItem";
+import Expenses from "./Components/Expenses";
+import Navbar from "./Components/Navbar";
 
 function App() {
   let expenses = [
     {
-      id:'e1',
-      title:'school Fee',
-      price:250,
-      date:new Date(2022,5,12)
+      id: "e1",
+      title: "school Fee",
+      price: 250,
+      date: new Date(2022, 5, 12),
     },
     {
-      id:'e2',
-      title:'Books',
-      price:450,
-      date:new Date(2022,4,21)
+      id: "e2",
+      title: "Books",
+      price: 450,
+      date: new Date(2022, 4, 21),
     },
     {
-      id:'e3',
-      title:'House Rent',
-      price:1250,
-      date:new Date(2021,1,1)
+      id: "e3",
+      title: "House Rent",
+      price: 1250,
+      date: new Date(2021, 1, 1),
     },
     {
-      id:'e4',
-      title:'Food',
-      price:540,
-      date:new Date(2020,8,22)
-    }
-
+      id: "e4",
+      title: "Food",
+      price: 540,
+      date: new Date(2020, 8, 22),
+    },
   ];
-  
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Sayyed Ashfaq</h1>
-        <Expenses item = {expenses} />
+        <Navbar />
 
-     
+        <Expenses item={expenses} />
       </header>
     </div>
   );
