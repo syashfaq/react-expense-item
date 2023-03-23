b6a5c83e4445fc24bf025943e8bccca8cf5b67e6
Ashfaq <syashfaq@gmail.comgit statuscleargit config --global user.name Ashfaqgit branch feature/initialcommitgit statuspwdgit config --global user.email syashfaq@gmail.com>
2023-03-23 17:02:27 +0530

    commited

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
+import React from "react";
 import './Expenses.css'
 import ExpenseItem from './ExpenseItem';
-
+import Card from '../UI/Card';
 function Expenses(props){
 return (
-    <div className='expenses'>
+    <Card className='expenses'>
         <ExpenseItem 
         date= {props.item[0].date} 
         title={props.item[0].title} 
@@ -20,7 +21,7 @@ return (
         date= {props.item[3].date} 
         title={props.item[3].title} 
         price={props.item[3].price}/>
-    </div>
+    </Card>
 )
 
 };
